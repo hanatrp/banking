b3f39dcadcb2acef12744d3c6297503585e91213
Hana Nur Aisyah <hananurtryn@gmail.com>
2024-08-15 23:29:07 +0700

    auth ui

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+"use server"
+
+export const signIn = async () => {
+  try {
+    
+  } catch (error) {
+    console.log('error : ' , error);
+    
+  }
+}
+export const signUp = async (userData : SignUpParams) => {
+  try {
+    
+  } catch (error) {
+    console.log('error : ' , error);
+    
+  }
+}
