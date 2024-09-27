59143f538c9cad7c437b15b058d2ae94af190f28
Hana Nur Aisyah <hananurtryn@gmail.com>
2024-09-27 17:17:34 +0700

    add loading

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className='bg-white text-black-1 font-semibold'>
+      loading...
+    </div>
+  )
+}
+
+export default loading
